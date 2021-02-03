517032e214b1a47ef0a97cc958a8880fe80fb567
Miquel Mancera <miquelmancera@ginebro.cat>
2021-02-03 18:43:20 +0100

    canvi a les funcions

--- js/seguent.js
+++ js/seguent.js
@@ -1,17 +1,40 @@
 function passaSeguent(objRebut){
-  let idObjPare = document.getElementById(objRebut.parentElement.id);
-  idObjPare.classList.remove("elementVisible");
-  idObjPare.classList.add("elementOcult");
-  
-  let coleccioArticles = document.getElementsByTagName("article");
-  let idObjPreguntaSeguent;
-
-  for (let index = 0; index < coleccioArticles.length; index++) {
-    if(coleccioArticles[index].id==idObjPare.id){
-      idObjPreguntaSeguent=coleccioArticles[index+1].id;
+  let idObjPregAct = objRebut.parentElement.parentElement.id; 
+  let colArticles = document.getElementsByTagName("article");
+  let idObjPregSeg;
+    for (let i = 0; i < colArticles.length; i++) {
+    if(colArticles[i].id == idObjPregAct){
+      idObjPregSeg = colArticles[i+1].id;
+      break;
     };
   }
 
-  document.getElementById(idObjPreguntaSeguent).classList.add("elementVisible");
-  document.getElementById(idObjPreguntaSeguent).classList.remove("elementOcult");
+  amagaElement(idObjPregAct);
+  mostraElement(idObjPregSeg);
+}
+
+
+function passaAnterior(objRebut){
+  let idObjPregAct = objRebut.parentElement.parentElement.id; 
+  let colArticles = document.getElementsByTagName("article");
+  let idObjPregAnt;
+    for (let i = 0; i < colArticles.length; i++) {
+    if(colArticles[i].id == idObjPregAct){
+      idObjPregAnt = colArticles[i-1].id;
+      break;
+    };
+  }
+  
+  amagaElement(idObjPregAct);
+  mostraElement(idObjPregAnt);
+}
+
+function mostraElement(idRebut){
+  document.getElementById(idRebut).classList.add("elementVisible");
+  document.getElementById(idRebut).classList.remove("elementOcult");
+}
+
+function amagaElement(idRebut){
+  document.getElementById(idRebut).classList.remove("elementVisible");
+  document.getElementById(idRebut).classList.add("elementOcult");
 }
